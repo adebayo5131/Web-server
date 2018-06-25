c990f936bff32cb6777447d652aeb014cc93d4fe
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-06-25 15:29:30 -0400

    Initialize repo

---
+++ public/pricing.js
@@ -0,0 +1,2 @@
+var items=["iphone X","iphone 7","iphone 6","iphone 5","Series 3","Fog Sport","Pink Sand Sport","Seashell Sport","MACBOOK PRO","MACBOOK Air", "MacBook Air 13.3","MACKBOOK Pro"];
+var prices=[1040.99,999.00,379.99,200.99,399.00,399.99,399.99,399.99,1999.00,903.99,999.99,2099.99];
