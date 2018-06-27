912a325ce4adff537f2ee21e0bc0889a78244500
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-06-27 01:43:20 -0400

    Extended mobile scaling

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -1,4 +1,4 @@
-var siteWidth = 1280/2;
+var siteWidth = 1280;
 var scale = screen.width /siteWidth
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
