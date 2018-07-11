505d4d641f8c4ed4ded0f8183a267967ca6d7718
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-11 15:07:36 -0400

    Update files

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -1,5 +1,15 @@
-var siteWidth = 1280;
+/* var siteWidth = 1280;
 var scale = screen.width /siteWidth
 
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+''); */
+
+$(window).resize(function() {
+    var mobileWidth =  (window.innerWidth > 0) ? 
+                        window.innerWidth : 
+                        screen.width;
+    var viewport = (mobileWidth > 360) ?
+                    'width=device-width, initial-scale=1.0' :
+                    'width=1200';
+    $("meta[name=viewport]").attr('content', viewport);
+}).resize(); 
 
