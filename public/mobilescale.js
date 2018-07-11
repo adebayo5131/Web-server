ee0605f851b9b7b03b94efc2652202d596e2f98a
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-11 14:12:22 -0400

    Update files

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -1,3 +1,25 @@
-var scale = window.outerWidth / 1280;
+var siteWidth = 1280;
+var scale = screen.width /siteWidth
 
-$('head').append('<meta name="viewport" content="width=1280, initial-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=0">');
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+(function(doc) {
+
+	var addEvent = 'addEventListener',
+	    type = 'gesturestart',
+	    qsa = 'querySelectorAll',
+	    scales = [1, 1],
+	    meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+	function fix() {
+		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+		doc.removeEventListener(type, fix, true);
+	}
+
+	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+		fix();
+		scales = [.25, 1.6];
+		doc[addEvent](type, fix, true);
+	}
+
+}(document));
