ccd932deadb3e22820aad405a2144634e01260cd
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-11 15:32:02 -0400

    Update files

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -1,4 +1,6 @@
 var siteWidth = 1280;
 var scale = screen.width /siteWidth
 
-document.querySelector('<meta name="viewport" content="width=1280, initial-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=0">');
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+
+
