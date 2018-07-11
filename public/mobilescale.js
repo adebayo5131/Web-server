94ff4bed98fb179de9e74c8b5eb62e3dbee4d77c
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-11 14:42:35 -0400

    Update files

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -1,5 +1,5 @@
 var siteWidth = 1280;
-var scale = screen.width /siteWidth
+var scale = screen.width /(siteWidth/2)
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
