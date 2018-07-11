faa405a273cbeac1e853943033bd119112ef0044
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2018-07-11 15:28:09 -0400

    Update files

--- public/mobilescale.js
+++ public/mobilescale.js
@@ -4,3 +4,6 @@ var scale = screen.width /siteWidth
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
 
+var scale = window.outerWidth / 1280;
+
+$('head').append('<meta name="viewport" content="width=1280, initial-scale=' + scale + ', maximum-scale=' + scale + ', user-scalable=0">');
